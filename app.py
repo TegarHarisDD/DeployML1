18b9865862822dcf37404865152fd01c649cd60a
Tegar Haris DD <113081151+TegarHarisDD@users.noreply.github.com>
2025-05-20 20:46:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,10 @@ page = st.sidebar.radio("Go to", ["About", "Single Prediction", "Bulk Prediction
 # ----------------------------------------
 if page == "About":
     st.title("Obesity Classification App")
+     st.markdown("""
+    **Purpose:**  
+    By Tegar Haris DD - A11.2022.14428
+    """)
     st.markdown(dataset_info)
     st.subheader("Tuned Model Accuracies")
     for model_name, acc in accuracy_tuned.items():
